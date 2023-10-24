850fc92b16d1ed84405c41922456c40d7cfa347b
sawyer315 <143819017+sawyer315@users.noreply.github.com>
2023-10-24 14:38:04 +0000

    big2

--- big_array.py
+++ big_array.py
@@ -1,4 +1,6 @@
 import random
+import math
+
 bigArray = []
 for i in range(0, 19):
   bigArray.append(random.randint(20, 90))
@@ -49,8 +51,64 @@ for i in range(0, 20):
   if 50 <= bigArray[i] and bigArray[i] <= 59:
     temp.append(bigArray[i])
 print("50's: ", temp)
-
+#11
+temp = []
 for i in range(0, 20):
-  if bigArray[i] % 4 == 0:
+  if int(bigArray[i]) % 4 == 0:
     temp.append(bigArray[i])
 print("Multiples of 4: ", temp)
+temp = 0
+for i in range(0, 20):
+  if bigArray[i] == 60:
+    temp = 1
+
+if temp == 1:
+  print("There is a 60 in the list.")
+else:
+  print("There is not a 60 in the list")
+
+reverseArray = []
+
+for i in range(0, 20):
+  reverseArray.insert(0, bigArray[i])
+
+print(reverseArray)
+
+if reverseArray == bigArray:
+  print("Palindromic")
+else:
+  print("Not palindromic")
+#14
+totAvg = 0
+temp = []
+for i in range(0, 20):
+  totAvg += bigArray[i]
+totAvg /= 21
+print("Average ", totAvg)
+for i in range(0, 20):
+  if bigArray[i] > totAvg:
+    temp.append(bigArray[i])
+
+print("Larger than average: ", temp)
+
+#15
+temp = []
+for i in range(0, 20):
+  if bigArray[i] % 2 == 0:
+    temp.append(bigArray[i])
+
+print("Even: ", temp)
+
+#16
+print("Reverse order: ", reverseArray)
+
+#18
+sum = 0
+for i in range(0, 20):
+  sum += bigArray[i]
+print("Sum: ", sum)
+
+#17
+temp = [0] * 21
+for i in range(0, 20):
+  
