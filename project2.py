19aa04e1f8c771b91f200105feaad1c380761a86
sawyer315 <143819017+sawyer315@users.noreply.github.com>
2023-12-18 15:36:38 +0000

    function

--- project2.py
+++ project2.py
@@ -9,17 +9,22 @@ price = 0.0
 label = ""
 total = 0.0
 select = 0
+count = 0
 
+def calc(a, r, n):
+  pr = round((a * r), 2)
+  la = str(a) + " of " + n
+  return pr, la
 while ready == 0:
   command = str(input("(add, remove, done) "))
   if command == "add":
     name = str(input("Name? "))
     amount = int(input("Amount? "))
     rate = float(input("Price of one? "))
-    price = round((amount * rate), 2)
-    label = str(amount) + " of " + name
+    price, label = calc(amount, rate, name)
     grocery.update({label:price})
     total += price
+    count += 1
   elif command == "remove":
     select = str(input("What item on the list? (Amount + name) "))
     grocery.pop(select)
@@ -29,3 +34,4 @@ while ready == 0:
     print("Try again ")
   print(grocery)
 print("Total cost: $", total)
+print("Avergage cost: $", total/count)
