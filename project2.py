3bee834399cf97c77cb3664e226832106eef3957
sawyer315 <143819017+sawyer315@users.noreply.github.com>
2023-12-19 15:36:53 +0000

    remake

--- project2.py
+++ project2.py
@@ -1,37 +1,31 @@
 #Grocery list
-
-grocery = {}
+grocery = []
 ready = 0
 name = ""
 amount = 0
-rate = 0.0
-price = 0.0
 label = ""
-total = 0.0
 select = 0
 count = 0
 
-def calc(a, r, n):
-  pr = round((a * r), 2)
-  la = str(a) + " of " + n
-  return pr, la
+
+#def findMost(list):
+  
+
 while ready == 0:
-  command = str(input("(add, remove, done) "))
+  command = str(input("(add, remove, done, sort) "))
   if command == "add":
     name = str(input("Name? "))
     amount = int(input("Amount? "))
-    rate = float(input("Price of one? "))
-    price, label = calc(amount, rate, name)
-    grocery.update({label:price})
-    total += price
+    label = name + ", " + str(amount)
+    grocery.append(label)
     count += 1
   elif command == "remove":
-    select = str(input("What item on the list? (Amount + name) "))
-    grocery.pop(select)
+    select = int(input("What position? "))
+    grocery.pop(select-1)
   elif command == "done":
     ready = 1
+  elif command == "sort":
+    grocery.sort()
   else:
     print("Try again ")
   print(grocery)
-print("Total cost: $", total)
-print("Avergage cost: $", total/count)
