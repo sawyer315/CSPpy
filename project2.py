7e8d30099f8d927f019543f423f69c1b697b2444
sawyer315 <143819017+sawyer315@users.noreply.github.com>
2023-12-20 15:32:33 +0000

    equal

--- project2.py
+++ project2.py
@@ -8,12 +8,21 @@ select = 0
 amounts = []
 most = 0
 
+targ = 0
+
+def isEqual(num1, num2):
+  return num1 == num2
 def findMost(list):
   for item in list:
     amounts.append(str(item)[-1])
   amounts.sort()
   return amounts[-1]
-
+def countNumber(list, target):
+  answer = 0
+  for item in list:
+    if isEqual(item, target) is True:
+      answer += 1
+  return answer
 while ready == 0:
   command = str(input("(add, remove, done, sort) "))
   if command == "add":
@@ -34,3 +43,5 @@ while ready == 0:
 for item in grocery:
   print(item)
 print("Highest amount: ", findMost(grocery))
+targ = input("How many of what amount? ")
+print(countNumber(amounts, targ))
