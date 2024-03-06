38e9ab75057f90e9827e57cb7d99713b72643003
sawyer315 <143819017+sawyer315@users.noreply.github.com>
2024-03-06 15:30:53 +0000

    sim

--- create_task.py
+++ create_task.py
@@ -29,7 +29,7 @@ player_score = 0
 cpu_score = 0
 single_player = 0
 gamemode = input('Gamemode? ')
-def cpu_decide(type):
+def cpu_decide(type, self, other):
   if type == 'Trusting':
     return 'Co'
   elif type == 'Distrustful':
@@ -46,7 +46,7 @@ def cpu_decide(type):
       elif temp == 2: return 'Be'
       else: return 'Ct'
     else:
-      return past_player_moves[-1]
+      return other[-1]
   elif type == 'Opposite':
     if turn == 1:
       temp = random.randint(1,3)
@@ -54,8 +54,8 @@ def cpu_decide(type):
       elif temp == 2: return 'Be'
       else: return 'Ct'
     else:
-      if past_player_moves[-1] == 'Co': return 'Be'
-      elif past_player_moves[-1] == 'Be': return 'Ct'
+      if other[-1] == 'Co': return 'Be'
+      elif other[-1] == 'Be': return 'Ct'
       else: return 'Co'
   elif type == 'Alternate':
     if turn == 1:
@@ -64,16 +64,15 @@ def cpu_decide(type):
       elif temp == 2: return 'Be'
       else: return 'Ct'
     else: 
-      if past_cpu_moves[-1] == 'Co': return 'Be'
-      elif past_cpu_moves[-1] == 'Ct': return 'Co'
+      if self[-1] == 'Co': return 'Be'
+      elif self[-1] == 'Ct': return 'Co'
       else: return 'Ct'
   elif type == 'Grudge':
     if turn == 1:
       return 'Co'
     else:
       temp = 0
-      print(past_player_moves)
-      for item in past_player_moves:
+      for item in other:
         if item == 'Be': temp +=1
         temp += 1
       if temp/(turn-1) == 1:
@@ -88,25 +87,24 @@ def cpu_decide(type):
     if turn == 1:
       return 'Co'
     elif turn == 2:
-      if past_player_moves[-1] == 'Co': return 'Be'
+      if other[-1] == 'Co': return 'Be'
       else: return 'Ct'
     else:
-      if past_player_moves[-1] == 'Co' and past_player_moves[-2] == 'Co': return 'Be'
-      elif past_player_moves[-1] == 'Be' and past_player_moves[-2] == 'Be': return 'Ct'
-      elif past_player_moves[-1] == 'Ct': return 'Co'
-      elif cpu_score>player_score: return past_cpu_moves[-1]
-      elif player_score>cpu_score: return past_player_moves[-1]
+      if other[-1] == 'Co' and other[-2] == 'Co': return 'Be'
+      elif other[-1] == 'Be' and other[-2] == 'Be': return 'Ct'
+      elif other[-1] == 'Ct': return 'Co'
+      else: return other[-1]
   elif type == 'Generous':
     if turn == 1 or turn == 2:
       return 'Co'
     else:
-      if past_player_moves[-1] == 'Be' and past_player_moves[-2] == 'Be': return 'Be'
+      if other[-1] == 'Be' and other[-2] == 'Be': return 'Ct'
       else: return 'Co'
   elif type == 'Wary':
     if turn == 1 or turn == 2:
       return 'Co'
     else:
-      if past_player_moves[-1] == 'Be' or past_player_moves[-2] == 'Be': return 'Ct'
+      if other[-1] == 'Be' or other[-2] == 'Be': return 'Ct'
       else: return 'Be'
   print(cpu_move)
 def calc_rewards(p1, p2):
@@ -141,7 +139,7 @@ if gamemode == 'Single':
     if player_move == 'Cooperate': player_move = 'Co'
     if player_move == 'Betray': player_move = 'Be'
     if player_move == 'Counter': player_move = 'Ct'
-    cpu_move = cpu_decide(cpu_type)
+    cpu_move = cpu_decide(cpu_type, past_cpu_moves, past_player_moves)
     print(cpu_move)
     if cpu_move == 'Be': print('CPU move: Betray')
     elif cpu_move == 'Co': print('CPU move: Cooperate')
@@ -180,48 +178,76 @@ if gamemode == 'Single':
 
 
 elif gamemode == 'Simulate':
-  p1_type =
-  p2_type =
+  p1_type = 0
+  p2_type = 1
+  type1_score,type2_score,type3_score,type4_score,type5_score,type6_score,type7_score,type8_score,type9_score,type10_score = 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0
   
-  for i in range(game_length):
-    print('================================================')
-    player_move = str(input('Your move? (Cooperate/Betray/Counter) '))
-    if player_move == 'Cooperate': player_move = 'Co'
-    if player_move == 'Betray': player_move = 'Be'
-    if player_move == 'Counter': player_move = 'Ct'
-    cpu_move = cpu_decide(cpu_type)
-    print(cpu_move)
-    if cpu_move == 'Be': print('CPU move: Betray')
-    elif cpu_move == 'Co': print('CPU move: Cooperate')
-    elif cpu_move == 'Ct': print('CPU move: Counter')
-    past_player_moves.append(player_move)
-    past_cpu_moves.append(cpu_move)
-    player_reward, cpu_reward = calc_rewards(player_move, cpu_move)
-    player_score += player_reward
-    cpu_score += cpu_reward
-    if player_reward == R:
-      print('You were rewarded')
-    elif player_reward == P:
-      print('You were punished')
-    elif player_reward == S:
-      print('You lost')
-    elif player_reward == T:
-      print('You win')
-    if cpu_reward == R:
-      print('CPU was rewarded')
-    elif cpu_reward == P:
-      print('CPU was punished')
-    elif cpu_reward == S:
-      print('CPU lost')
-    elif cpu_reward == T:
-      print('CPU wins')
-    print('Current score:')
-    print('Player: ', player_score)
-    print('CPU score: ', cpu_score)
-    turn +=1
-  print('============================================')
-  print('GAME OVER')
-  if player_score>cpu_score: print('You win!')
-  elif player_score<cpu_score: print('You lose!')
-  elif player_score == cpu_score: print('Tie!')
-  print('Final score: ',player_score,', ',cpu_score)
+
+  for i in range(99):
+    p1_type +=1
+    if p1_type == 11:
+      p1_type = 1
+      p2_type+=1
+    for i in range(game_length):
+      print('================================================')
+      player_move = cpu_decide(p1_type,past_player_moves,past_cpu_moves)
+      cpu_move = cpu_decide(p2_type,past_cpu_moves,past_player_moves)
+      past_player_moves.append(player_move)
+      past_cpu_moves.append(cpu_move)
+      player_reward, cpu_reward = calc_rewards(player_move, cpu_move)
+      player_score += player_reward
+      cpu_score += cpu_reward
+      print(player_reward,' ',cpu_reward,' ',player_move,' ',cpu_move)
+      print('Player: ', player_score)
+      print('CPU score: ', cpu_score)
+      turn +=1
+    print('============================================')
+    print('GAME OVER')
+    if player_score>cpu_score:
+      if p1_type == 1: type1_score +=1
+      elif p1_type == 2: type2_score +=1
+      elif p1_type == 3: type3_score +=1
+      elif p1_type == 4: type4_score +=1
+      elif p1_type == 5: type5_score +=1
+      elif p1_type == 6: type6_score +=1
+      elif p1_type == 7: type7_score +=1
+      elif p1_type == 8: type8_score +=1
+      elif p1_type == 9: type9_score +=1
+      elif p1_type == 10: type10_score +=1
+    elif cpu_score>player_score:
+      if p2_type  ==  1: type1_score +=1
+      elif p2_type == 2: type2_score +=1
+      elif p2_type == 3: type3_score +=1
+      elif p2_type == 4: type4_score +=1
+      elif p2_type == 5: type5_score +=1
+      elif p2_type == 6: type6_score +=1
+      elif p2_type == 7: type7_score +=1
+      elif p2_type == 8: type8_score +=1
+      elif p2_type == 9: type9_score +=1
+      elif p2_type ==10:type10_score +=1
+    else: 
+      if p1_type == 1: type1_score +=0.5
+      elif p1_type == 2: type2_score +=.5
+      elif p1_type == 3: type3_score +=.5
+      elif p1_type == 4: type4_score +=.5
+      elif p1_type == 5: type5_score +=.5
+      elif p1_type == 6: type6_score +=.5
+      elif p1_type == 7: type7_score +=.5
+      elif p1_type == 8: type8_score +=.5
+      elif p1_type == 9: type9_score +=.5
+      elif p1_type == 10: type10_score +=.5
+      if p2_type  ==  1: type1_score +=.5
+      elif p2_type == 2: type2_score +=.5
+      elif p2_type == 3: type3_score +=.5
+      elif p2_type == 4: type4_score +=.5
+      elif p2_type == 5: type5_score +=.5
+      elif p2_type == 6: type6_score +=.5
+      elif p2_type == 7: type7_score +=.5
+      elif p2_type == 8: type8_score +=.5
+      elif p2_type == 9: type9_score +=.5
+      elif p2_type ==10:type10_score +=.5
+print('Trusting: ', type1_score)
+print('Distrusting: ', type2_score)
+print('Random', type3_score)
+print('Copycat', type4_score)
+print('Opposite', type5_score)
