26c0f3898d688cffefca246ecc106da35b4cac25
sawyer315 <143819017+sawyer315@users.noreply.github.com>
2024-05-02 16:08:27 +0000

    unix

--- 2018_7.py
+++ 2018_7.py
@@ -1,52 +1,95 @@
 unix = int(input('UNIX time: '))
 
-years = 0
-leap = 2
-while unix >= 31536000:
-  if years % 4 == 0 and (years % 100 != 0 or years % 400 == 0):
-    unix -= 31622400
-  else:
-    unix -= 31536000
-  years +=1
-  leap+=1
-print('years',years)
-
-days = 0
-while unix >= 86400:
-  unix -= 86400
-  days += 1
-print('days',days)
+unix = int(unix/60)
+day_half = 'AM'
 hours = 0
-while unix >= 3600:
-  unix -= 3600
-  hours += 1
-print('hours',hours)
-minutes = 0
+days = 0
+month = 'Jan'
+year = 1970
+lmonth = ['Jan',  'Mar', 'May',
+          'Jul', 'Aug', 'Oct', 'Dec']
+smonth = ['Apr', 'Jun', 'Sep', 'Nov']
+allmonths = ['Jan', 'Feb', 'Mar',
+             'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+year_done = False
+while year_done is False:
+  if year % 4 == 0:
+    if unix >= 527040:
+      unix -= 527040
+      year += 1
+    else:
+      year_done = True
+  else:
+    if unix >= 525600:
+      unix -= 525600
+      year += 1
+    else:
+      year_done = True
+
+while unix >= 1440:
+  days += 1
+  unix -= 1440
+
+month_done = False
+
+if year % 4 == 0:
+  while month_done is False:
+    if month in lmonth:
+      if days >= 31:
+        days -= 31
+        allmonths.pop(0)
+        month = allmonths[0]
+      else: month_done = True
+    elif month in smonth:
+      if days >= 30:
+        days -= 30
+        allmonths.pop(0)
+        month = allmonths[0]
+      else: month_done = True
+    else:
+      if days >= 29:
+        days -= 29
+        allmonths.pop(0)
+        month = allmonths[0]
+      else: month_done = True
+
+  
+else:
+  while month_done is False:
+    if month in lmonth:
+      if days >= 31:
+        days -= 31
+        allmonths.pop(0)
+        month = allmonths[0]
+      else: month_done = True
+    elif month in smonth:
+      if days >= 30:
+        days -= 30
+        allmonths.pop(0)
+        month = allmonths[0]
+      else: month_done = True
+    else:
+      if days >= 28:
+        days -= 28
+        allmonths.pop(0)
+        month = allmonths[0]
+      else: month_done = True
+
 while unix >= 60:
   unix -= 60
-  minutes += 1
-print('mins', minutes)
-print('seconds',unix)
-year = 1970 + years
-print('year',year)
-month = 0
-if leap % 4 == 0:
-  days_in_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  print('Leap Year')
-else:
-  days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-day = 1
-while days >= 0:
-  day = days+1
-  days -= days_in_month[0]
-  month += 1
-  if days_in_month != []:
-    days_in_month.pop(0)
-print(month)
-day_half = 'AM'
+  hours += 1
+
 if hours >= 12:
   hours -= 12
   day_half = 'PM'
-if hours == 0: hours = 12
+if hours == 0:
+  hours = 12
+days +=1
 
-print(f'{hours:02d}:{minutes:02d} {day_half}  {month:02d}/{day:02d}/{year}')
+print(year)
+print(days+1)
+print(month)
+print(hours)
+print(unix)
+
+print(f'{hours:02d}:{unix:02d} {day_half}  {month}/{days:02d}/{year}')
