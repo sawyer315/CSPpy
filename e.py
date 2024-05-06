9c0d038ea6d2db8345c67afa2e059c1db0f1343a
sawyer315 <143819017+sawyer315@users.noreply.github.com>
2024-05-06 15:21:04 +0000

    Magnum Opus

--- e.py
+++ e.py
@@ -4,14 +4,42 @@ es = 1
 bigE = 1
 littleE = 0
 space = es
-while True:
-  space = ' '*es + ' '* (littleE%3)
+countE = 1
+E_is_the_best_letter = True
+while E_is_the_best_letter:
+  space = ' '*es + ' '* (littleE%4)
   print(space+'e')
   es += bigE
-  if es == 20:
+  if es == 25:
     bigE = -1
   if es == 1:
     bigE = 1
   littleE += 1
   littleE *= -1
   time.sleep(0.1)
+  if littleE == 400:
+    print('aahhhhhhhh')
+  if countE%100 == 0 :
+    linE = 'eeeeeeeeeeeeeeeeeeeee'
+    shortE = 'ee'
+    print(linE)
+    print(linE)
+    print(shortE)
+    print(shortE)
+    print(shortE)
+    print(shortE)
+    print(shortE)
+    print(shortE)
+    print(shortE)
+    print(linE)
+    print(linE)
+    print(shortE)
+    print(shortE)
+    print(shortE)
+    print(shortE)
+    print(shortE)
+    print(shortE)
+    print(shortE)
+    print(linE)
+    print(linE)
+  countE +=1
